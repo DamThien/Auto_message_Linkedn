af211398937ca2d4207a1f203329644b71a18ed4
thien <thiendam052@gmail.com>
2023-07-27 14:36:34 +0700

    change some code

--- scraper.py
+++ scraper.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 
+from selenium.webdriver.chrome.service import Service
 from time import sleep
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
@@ -25,7 +25,7 @@ line = credential.readlines()
 username = line[0]
 password = line[1]
 print('- Finish importing the login credentials')
-sleep(1)
+sleep(2)
 
 # Task 1.2: Key in login credentials
 email_field = driver.find_element(By.ID, 'username').send_keys(username)
@@ -42,7 +42,7 @@ signin_field = driver.find_element(
     By.XPATH, '//*[@id="organic-div"]/form/div[3]/button')
 signin_field.click()
 print('- Finish Task 1: Login to Linkedin')
-sleep(15)
+sleep(1)
 
 
 # Đường dẫn đến trang group của bạn
@@ -54,7 +54,7 @@ print('Truy cập vào trang group của bạn')
 # Định vị phần tử chứa danh sách các thành viên trong nhóm
 
 member_names = []
-for abcsd in driver.find_elements(By.CSS_SELECTOR,".artdeco-entity-lockup__title.ember-view"):
+for abcsd in driver.find_elements(By.CSS_SELECTOR, ".artdeco-entity-lockup__title.ember-view"):
     print(abcsd.text)
     member_names.append(abcsd.text)
 print("Định vị phần tử chứa danh sách các thành viên trong nhóm")
@@ -67,6 +67,7 @@ for member in member_names:
             (By.CSS_SELECTOR, '.artdeco-entity-lockup__title.ember-view'))
     )
     print(member_container)
+
     # Lấy tên của thành viên từ phần tử đã định vị.
     member_name = member
     print(member)
@@ -81,7 +82,7 @@ for member in member_names:
     # Tìm ô tin nhắn
     message_box_click = driver.find_element(
         By.XPATH, f"//*[@aria-label='Message {member}']")
-    
+
     # Move the mouse cursor to the element and then click
     action = ActionChains(driver)
     action.move_to_element(message_box_click).click().perform()
@@ -91,7 +92,7 @@ for member in member_names:
 
     # Find the input field for composing the message
     message_input = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '.msg-form__contenteditable'))
+        EC.presence_of_element_located(By.CSS_SELECTOR, '.msg-form__contenteditable')
     )
 
     # Paste the customized message into the input field
@@ -100,7 +101,7 @@ for member in member_names:
 
     # Wait for a short duration (adjust this sleep time if needed)
     sleep(1)
-    
+
     # Find the "Send" button and click it
     send_click = driver.find_element(
         By.CSS_SELECTOR, ".msg-form__send-button.artdeco-button.artdeco-button--1")
