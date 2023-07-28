684fcd886d5c90dc9358d588ade0031d033572e6
thien <thiendam052@gmail.com>
2023-07-28 10:48:57 +0700

    Code can send messages to all members

--- scraper.py
+++ scraper.py
@@ -1,15 +1,15 @@
-# Import libraries and packages for the project
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-from selenium.webdriver.chrome.service import Service
-from time import sleep
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 import csv
+from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+# Hiện tại có thể nhắn tin cho nhiều người nhưng đang gặp một vấn đề đó là vẫn không thể ấn nút x để tắt thành viên đã nhắn tin.
+# Import libraries and packages for the project
+
 print('- Finish importing packages')
 
 driver = webdriver.Chrome()
@@ -42,8 +42,7 @@ signin_field = driver.find_element(
     By.XPATH, '//*[@id="organic-div"]/form/div[3]/button')
 signin_field.click()
 print('- Finish Task 1: Login to Linkedin')
-sleep(1)
-
+sleep(15)
 
 # Đường dẫn đến trang group của bạn
 group_url = "https://www.linkedin.com/groups/14285566/manage/membership/members/"
@@ -59,73 +58,71 @@ for abcsd in driver.find_elements(By.CSS_SELECTOR, ".artdeco-entity-lockup__titl
     member_names.append(abcsd.text)
 print("Định vị phần tử chứa danh sách các thành viên trong nhóm")
 
+# Get your LinkedIn account's name
+your_name = "Dam Long Thien"
+
 # Gửi tin nhắn cho từng thành viên
 for member in member_names:
-    # Định vị phần tử chứa tên của thành viên.
-    member_container = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.CSS_SELECTOR, '.artdeco-entity-lockup__title.ember-view'))
-    )
-    print(member_container)
+    # Skip sending a message to your own account
+    if member == your_name:
+            continue
+    try:
+        # Tìm ô tin nhắn trên trang group
+        message_box_click = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, f"//*[@aria-label='Message {member}']"))
+        )
+        # Scroll to the message button to bring it into view
+        driver.execute_script(
+            "arguments[0].scrollIntoView();", message_box_click)
 
-    # Lấy tên của thành viên từ phần tử đã định vị.
-    member_name = member
-    print(member)
-    message = open('message.txt')
-    # Read the message from the file with 'utf-8' encoding
-    with open('message.txt', encoding='utf-8') as message_file:
-        customized_message = message_file.read()
-    send_message = "Hi " + member.split()[0] + ",\n" + customized_message
-    print(customized_message)
-    print(send_message)
+        # Move the mouse cursor to the element and then click
+        action = ActionChains(driver)
+        action.move_to_element(message_box_click).click().perform()
 
-    # Tìm ô tin nhắn
-    message_box_click = driver.find_element(
-        By.XPATH, f"//*[@aria-label='Message {member}']")
+        # Wait for a short duration (adjust this sleep time if needed)
+        sleep(1)
 
-    # Move the mouse cursor to the element and then click
-    action = ActionChains(driver)
-    action.move_to_element(message_box_click).click().perform()
+        # Find the input field for composing the message
+        message_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, '.msg-form__contenteditable'))
+        )
+        
+        # Compose your customized message for the specific member
+        message = open('message.txt')
+        with open('message.txt', encoding='utf-8') as message_file:
+            customized_message = message_file.read()
+        send_message = "Hi " + member.split()[0] + ",\n" + customized_message
 
-    # Wait for a short duration (adjust this sleep time if needed)
-    sleep(1)
+        # Paste the customized message into the input field
+        action = ActionChains(driver)
+        action.click(message_input).send_keys(send_message).perform()
 
-    # Find the input field for composing the message
-    message_input = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(By.CSS_SELECTOR, '.msg-form__contenteditable')
-    )
+        # Wait for a short duration (adjust this sleep time if needed)
+        sleep(1)
 
-    # Paste the customized message into the input field
-    action = ActionChains(driver)
-    action.click(message_input).send_keys(send_message).perform()
+        # Find the "Send" button and click it
+        send_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, ".msg-form__send-button.artdeco-button.artdeco-button--1"))
+        )
+        send_button.click()
 
-    # Wait for a short duration (adjust this sleep time if needed)
-    sleep(1)
+        # Wait for a short duration (adjust this sleep time if needed)
+        sleep(3)
 
-    # Find the "Send" button and click it
-    send_click = driver.find_element(
-        By.CSS_SELECTOR, ".msg-form__send-button.artdeco-button.artdeco-button--1")
-    action = ActionChains(driver)
-    action.move_to_element(send_click).click().perform()
+    except Exception as e:
+        print(f"Error sending message to {member}: {e}")
 
-    # Wait for a short duration (adjust this sleep time if needed)
-    sleep(3)
+    finally:
+        close_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, '//button[@class="msg-overlay-bubble-header__control artdeco-button artdeco-button--circle artdeco-button--muted artdeco-button--1 artdeco-button--tertiary ember-view"]'))
+        )
+        close_button.click()
+        print("Đã gửi tin nhắn cho",member)
+        sleep(2)
 
-    # Go back to the main group members page after sending the message
-    driver.get(group_url)
-    sleep(5)
-
-    # Find the next member element in the list
-    next_member = driver.find_element(
-        By.XPATH, f"//*[text()='{member}']/ancestor::li/following-sibling::li//button[@aria-label='Message']")
-
-    # Scroll the element into view
-    driver.execute_script("arguments[0].scrollIntoView(true);", next_member)
-
-    # Click on the next member's message button
-    next_member.click()
-
-    # Wait for a short duration (adjust this sleep time if needed)
-    sleep(3)
-
-    # 2222
+# Kết thúc khi đã gửi tin nhắn cho toàn bộ thành viên
+print("Đã gửi tin nhắn cho toàn bộ thành viên trong nhóm")
